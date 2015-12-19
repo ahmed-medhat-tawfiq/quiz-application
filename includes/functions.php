100c8e435befa86a245e4e3dbfbf9ab05c8e6e9e
medhat93 <ahmed.medhat793@yahoo.com>
2015-12-19 20:28:39 +0200

    Update functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -4,3 +4,17 @@ include("db_connection.php");
 
 
 ?>
+
+<?php
+  $connection=0; 
+  function dbconnect($dbhost, $dbuser, $dbpass, $dbname)
+	{ global $connection;
+	  $connection = mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
+	  if(mysqli_connect_errno()){
+		  die("Database connection failed:".mysqli_connect_error()."(".mysqli_connect_errno().")");
+	}}
+  function closeconnect()
+	{global $connection;
+	 mysqli_close($connection);}
+	
+?>
